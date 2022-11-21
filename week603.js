dd10a441ecfa88e2c736a98372d2ed11332bfb71
Napatsutthitham <napatsutthitham@gmail.com>
2022-11-21 09:57:14 +0700

    updata

---
+++ week603.js
@@ -0,0 +1,3 @@
+const obj = {a:1,b:2,c:3}
+for(let prop in obj)
+    console.log(obj[prop])
---
+++ week604.js
@@ -0,0 +1,4 @@
+let province = {b:"Trat",p: "Chantaburi"}
+
+for (let thai in province ){
+     console.log(province[thai])}
